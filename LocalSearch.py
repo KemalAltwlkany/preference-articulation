1f462c487af57ad4ff745c1e15480dbd91d0e9e1
Kemal Altwlkany <waykiki13@gmail.com>
2020-03-05 23:27:56 +0100

    Minor corrections on the uni-criteria LocalSearch.

--- LocalSearch.py
+++ LocalSearch.py
@@ -3,26 +3,24 @@ from Solution import *
 
 
 class LocalSearch:
-    delta = 0.1
 
     def __init__(self, init_sol=None, objectives=None, delta=None, max_iter=None, constraints=None):
         self.init_sol = init_sol
         self.max_iter = max_iter
         self.objectives = objectives
         self.constraints = constraints
-        LocalSearch.delta = delta
+        self.delta = delta
 
     # function returns a list of vectors containing the first input x's neighborhood.
     # noinspection DuplicatedCode
-    @staticmethod
-    def generate_neighborhood(x=[], neighborhood=[], current_span=[]):
+    def generate_neighborhood(self, x=[], neighborhood=[], current_span=[]):
         if len(x) == 1:
             span1 = list(current_span)
-            span1.append(x[0] - LocalSearch.delta)
+            span1.append(x[0] - self.delta)
             span2 = list(current_span)
             span2.append(x[0])
             span3 = list(current_span)
-            span3.append(x[0] + LocalSearch.delta)
+            span3.append(x[0] + self.delta)
             neighborhood.append(span1)
             neighborhood.append(span2)
             neighborhood.append(span3)
@@ -30,15 +28,21 @@ class LocalSearch:
         else:
             new_span = list(current_span)
             new_span.append(x[0])
-            neighborhood = LocalSearch.generate_neighborhood(x=x[1:], neighborhood=neighborhood, current_span=new_span)
-            new_span[-1] = new_span[-1] - LocalSearch.delta
-            neighborhood = LocalSearch.generate_neighborhood(x=x[1:], neighborhood=neighborhood, current_span=new_span)
-            new_span[-1] = new_span[-1] + 2 * LocalSearch.delta
-            neighborhood = LocalSearch.generate_neighborhood(x=x[1:], neighborhood=neighborhood, current_span=new_span)
+            neighborhood = self.generate_neighborhood(x=x[1:], neighborhood=neighborhood, current_span=new_span)
+            new_span[-1] = new_span[-1] - self.delta
+            neighborhood = self.generate_neighborhood(x=x[1:], neighborhood=neighborhood, current_span=new_span)
+            new_span[-1] = new_span[-1] + 2 * self.delta
+            neighborhood = self.generate_neighborhood(x=x[1:], neighborhood=neighborhood, current_span=new_span)
         return neighborhood
 
     def sort_neighborhood(self, neighborhood):
         pass
+
+    def evaluate_solution(self, sol):
+        y = []
+        for f in self.objectives:
+            y.append(f(sol.x))
+        return y
 
     # WORKS FOR SINGLE OBJECTIVE PROBLEMS ONLY, NEEDS TO BE REWORKED
     @staticmethod
@@ -47,19 +51,19 @@ class LocalSearch:
 
     def search(self):
         it = 0
+        y = self.evaluate_solution(self.init_sol)
+        self.init_sol.set_y(y)
         curr_sol = self.init_sol
         while it < self.max_iter:
             it = it + 1
             prev_sol = copy.deepcopy(curr_sol)
-            neighborhood_search_space = LocalSearch.generate_neighborhood(curr_sol.x)
+            neighborhood_search_space = self.generate_neighborhood(curr_sol.x)
             neighborhood_search_space.remove(curr_sol.x)
             neighborhood_solutions = []
 
             for x in neighborhood_search_space:
                 neighborhood_solutions.append(Solution(x))
-                y = []
-                for f in self.objectives:
-                    y.append(f(x))
+                y = self.evaluate_solution(neighborhood_solutions[-1])
                 neighborhood_solutions[-1].set_y(y)
 
             neighborhood_solutions.sort(key=LocalSearch.compare_solutions)
@@ -77,7 +81,6 @@ def f1(x):
 
 def main():
     init_sol = Solution([16])
-    #FIRST ITERATION VALUE OF INITIAL SOLUTION IS MISSING!
     delta = 0.1
     max_iter = 500
     objectives = [f1]
