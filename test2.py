9652d49e944584d515829130ab840e042daba919
Kemal Altwlkany <waykiki13@gmail.com>
2020-03-19 00:41:25 +0100

    Added a lot of functionality to the visualization. Around 5-6 benchmark problems are there now.

--- test2.py
+++ test2.py
@@ -1,8 +1,4 @@
 import numpy as np
 
-x1 = np.linspace(-5, 5, 10)
-x2 = np.linspace(-5, 5, 10)
-x = np.linspace((-10, -5), (10, 5), 10)
-print(x)
-print(type(x))
-print(x[0][1])
+x1 = [-3, -4]
+print(-x1)
